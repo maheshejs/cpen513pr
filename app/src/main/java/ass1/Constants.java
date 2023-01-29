4ebadd3c9286937a75259fc8053e92379ab9f4ac
Joseph Maheshe <maheshejs@gmail.com>
2023-01-28 23:52:22 -0800

    ASS1 : Clean up Constants - Replace array of Colors (from javafx.scene.paint) by array of Strings representing colors

--- app/src/main/java/ass1/Constants.java
+++ app/src/main/java/ass1/Constants.java
@@ -1,5 +1,4 @@
 package ass1;
-import javafx.scene.paint.Color;
 public class Constants{
     private Constants () {}
     static final double CELL_WIDTH   = 25;
@@ -7,30 +6,8 @@ public class Constants{
     static final String[] TERMINAL_COLORS = {   "red", "yellow", "green", 
                                                 "orange", "purple", "sienna",
                                                 " gray", "magenta"  };
-    static final Color[] COLORS = { 
-                                    Color.RED, 
-                                    Color.YELLOW, 
-                                    Color.GREEN, 
-                                    Color.ORANGE,
-                                    Color.PURPLE, 
-                                    Color.SIENNA, 
-                                    Color.GRAY, 
-                                    Color.MAGENTA
-                                  };
     enum Algo {
         LEE_MOORE,
         A_STAR,
     }
 }
-                        
-                        /*
-                        if (wire == 0 && i == NUM_ITERATIONS-1)
-                        {
-                            System.out.println(leafNode);
-                            Rectangle rect = new Rectangle(leafNode.getX() * CELL_WIDTH, leafNode.getY() * CELL_HEIGHT,
-                                                                CELL_WIDTH, CELL_HEIGHT);
-                            rect.setStroke(Color.BLUE);
-                            rect.setFill(Color.BLACK);
-                            grid.getChildren().add(rect);
-                        }
-                        */
